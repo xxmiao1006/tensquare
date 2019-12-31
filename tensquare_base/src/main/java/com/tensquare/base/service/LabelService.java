d829bb7f2a49e411263dbc974634ace8f5e8c427
xxmiao1006 <1151856023@qq.com>
2019-12-31 18:37:31 +0800

    condition search

--- tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
+++ tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
@@ -4,10 +4,14 @@ package com.tensquare.base.service;
 import com.tensquare.base.dao.LabelDao;
 import com.tensquare.base.pojo.Label;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import util.IdWorker;
 
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class LabelService {
@@ -19,42 +23,81 @@ public class LabelService {
 
     /**
      * 查询全部标签
+     *
      * @return
      */
-    public List<Label> findAll(){
+    public List<Label> findAll() {
         return labelDao.findAll();
     }
 
     /**
      * 根据ID查询标签
+     *
      * @return
      */
-    public Label findById(String id){
+    public Label findById(String id) {
         return labelDao.findById(id).get();
     }
 
     /**
      * 增加标签
+     *
      * @param label
      */
-    public void add(Label label){
-        label.setId( idWorker.nextId()+"" );//设置ID
+    public void add(Label label) {
+        label.setId(idWorker.nextId() + "");//设置ID
         labelDao.save(label);
     }
 
     /**
      * 修改标签
+     *
      * @param label
      */
-    public void update(Label label){
+    public void update(Label label) {
         labelDao.save(label);
     }
 
     /**
      * 删除标签
+     *
      * @param id
      */
-    public void deleteById(String id){
+    public void deleteById(String id) {
         labelDao.deleteById(id);
     }
+
+    /**
+     * 条件查询
+     *
+     * @param searchMap
+     * @return
+     */
+    public List<Label> findSearch(Map searchMap) {
+        Specification specification = createSpecification(searchMap);
+        return labelDao.findAll(specification);
+    }
+
+    /**
+     * 构建查询条件
+     *
+     * @param searchMap
+     * @return
+     */
+    private Specification<Label> createSpecification(Map searchMap) {
+        return (root, criteriaQuery, cb) -> {
+            List<Predicate> predicateList = new ArrayList<>();
+            if (searchMap.get("labelname") != null && !"".equals(searchMap.get("labelname"))) {
+                predicateList.add(cb.like(root.get("labelname").as(String.class), "%" + searchMap.get("labelname") + "%"));
+            }
+            if (searchMap.get("state") != null && !"".equals(searchMap.get("state"))) {
+                predicateList.add(cb.equal(root.get("state").as(String.class), searchMap.get("state")));
+            }
+            if (searchMap.get("recommend") != null && !"".equals(searchMap.get("recommend"))) {
+                predicateList.add(cb.equal(root.get("recommend").as(String.class), searchMap.get("recommend")));
+            }
+            return cb.and(predicateList.toArray(new Predicate[predicateList.size()]));
+        };
+    }
+
 }
