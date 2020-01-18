8ba3f6af76daa1ee288a1408a29d797ce0fde406
xxmiao1006 <1151856023@qq.com>
2020-01-18 10:42:03 +0800

    feign-hystrix

--- tensquare_qa/src/main/java/com/tensquare/qa/client/LabelClient.java
+++ tensquare_qa/src/main/java/com/tensquare/qa/client/LabelClient.java
@@ -1,5 +1,6 @@
 package com.tensquare.qa.client;
 
+import com.tensquare.qa.client.impl.LabelClientImpl;
 import entity.Result;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @FeignClient注解用于指定从哪个服务中调用功能 ，注意 里面的名称与被调用的服务名保持一致，并且不能包含下划线。
  * @RequestMapping注解用于对被调用的微服务进行地址映射。注意 @PathVariable注解一定要指定参数名称，否则出错
  */
-@FeignClient("tensquare-base")
+@FeignClient(value = "tensquare-base", fallback = LabelClientImpl.class)
 public interface LabelClient {
     @RequestMapping(value = "/label/{id}", method = RequestMethod.GET)
     Result findById(@PathVariable("id") String id);
